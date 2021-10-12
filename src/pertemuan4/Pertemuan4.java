260191b31ac37fef001a6abd144d95300771dbb3
dayuza <bocilumam@gmail.com>
2021-10-12 20:38:56 +0700

    Java_Pertemuan4

---
+++ src/pertemuan4/Pertemuan4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan4;
+import ui.Home;
+
+/**
+ *
+ * @author Dayuza
+ */
+public class Pertemuan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Home fHome = new Home();
+    fHome.setVisible(true);
+    }
+    
+}
